46a15c14301c92149a6d9524b5f3a536cd007921
sailendrachettri <sailendra9083@gmail.com>
2022-12-12 21:16:57 +0530

    common location weather information added in the form of table

--- js/tableData.js
+++ js/tableData.js
@@ -1,9 +1,9 @@
 const op = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '8eb942e62fmshb1157e8fb54e693p1580e5jsn94ed81c94ed3',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'cf94c8a7a4msh21ad4c475c254b3p1d1214jsn4729a9ee2e44',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
 };
 
 // api alternative 1: 31969a1614msh88ac34bc8e6c48bp1b89e5jsn30b700019bd0
@@ -16,24 +16,186 @@ const op = {
 // api ninjs - GGX8hA4PFKTlXOlNuu6kyw==6tVGGxtqLVCe6rg8
 
 
-fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Gangtok', op)
+
+
+// __________________________________LOCATION INFO FOR SEARCH QUERYES STARTED____________________________________
+const weatherInfo = (location) => {
+    cityName.innerHTML = location.toUpperCase();
+
+    fetch(`https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${location}`, op)
+        .then(response => response.json())
+        .then((response) => {
+
+            searchCityTemp.innerHTML = response.temp
+            searchCityMax_temp.innerHTML = response.max_temp
+            searchCityMin_temp.innerHTML = response.min_temp
+            searchCityWind_speed.innerHTML = response.wind_speed
+            searchCityHumidity.innerHTML = response.humidity
+
+
+            //    console.log(response)
+        })
+        .catch(err => console.error(err));
+}
+
+submitLocationBtn.addEventListener('click', (e) => {
+    e.preventDefault(); // prevents from reloading webpage
+    // if user submit empty field then the location will be "Gangtok" by default
+    weatherInfo((searchBox.value == "") ? "Gangtok" : searchBox.value); // just a if-else in one line
+
+    searchBox.value = "";  //   optional fearure
+});
+
+weatherInfo("Gangtok");
+
+// __________________________________LOCATION INFO FOR SEARCH QUERYES STARTED____________________________________
+
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Pakyong', op)
 	.then(response => response.json())
 	.then((response) => {
 
-		gangtokHumidity.innerHTML = response.humidity
-		gangtokMax_temp.innerHTML = response.max_temp
-		gangtokMin_temp.innerHTML = response.min_temp
-		gangtokTemp.innerHTML = response.temp
-		gangtokWind_speed.innerHTML = response.wind_speed
+		pakyongTemp.innerHTML = response.temp
+		pakyongMax_temp.innerHTML = response.max_temp
+		pakyongMin_temp.innerHTML = response.min_temp
+		pakyongWind_speed.innerHTML = response.wind_speed
+		pakyongHumidity.innerHTML = response.humidity
 
 
-		   console.log(response)
+		   //    console.log(response)
 	})
 .catch(err => console.error(err));
 
-// ____________________________________TABLE DATA STARTED____________________________________ 
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Namchi', op)
+	.then(response => response.json())
+	.then((response) => {
 
-// let locationName = document.getElementById("searchBox").value;
-// console.log(locationName);
+		namchiTemp.innerHTML = response.temp
+		namchiMax_temp.innerHTML = response.max_temp
+		namchiMin_temp.innerHTML = response.min_temp
+		namchiWind_speed.innerHTML = response.wind_speed
+		namchiHumidity.innerHTML = response.humidity
 
-// ____________________________________TABLE DATA ENDED________________________________
+
+		   //    console.log(response)
+	})
+.catch(err => console.error(err));
+
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Mangan', op)
+	.then(response => response.json())
+	.then((response) => {
+
+		manganTemp.innerHTML = response.temp
+		manganMax_temp.innerHTML = response.max_temp
+		manganMin_temp.innerHTML = response.min_temp
+		manganWind_speed.innerHTML = response.wind_speed
+		manganHumidity.innerHTML = response.humidity
+
+
+		   //    console.log(response)
+	})
+.catch(err => console.error(err));
+
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Singtam', op)
+	.then(response => response.json())
+	.then((response) => {
+
+		singtamTemp.innerHTML = response.temp
+		singtamMax_temp.innerHTML = response.max_temp
+		singtamMin_temp.innerHTML = response.min_temp
+		singtamWind_speed.innerHTML = response.wind_speed
+		singtamHumidity.innerHTML = response.humidity
+
+
+		   //    console.log(response)
+	})
+.catch(err => console.error(err));
+
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Jorethang', op)
+	.then(response => response.json())
+	.then((response) => {
+
+		jorethangTemp.innerHTML = response.temp
+		jorethangMax_temp.innerHTML = response.max_temp
+		jorethangMin_temp.innerHTML = response.min_temp
+		jorethangWind_speed.innerHTML = response.wind_speed
+		jorethangHumidity.innerHTML = response.humidity
+
+
+		   //    console.log(response)
+	})
+.catch(err => console.error(err));
+
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Rangpo', op)
+	.then(response => response.json())
+	.then((response) => {
+
+		rangpoTemp.innerHTML = response.temp
+		rangpoMax_temp.innerHTML = response.max_temp
+		rangpoMin_temp.innerHTML = response.min_temp
+		rangpoWind_speed.innerHTML = response.wind_speed
+		rangpoHumidity.innerHTML = response.humidity
+
+
+		   //    console.log(response)
+	})
+.catch(err => console.error(err));
+
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Soreng', op)
+	.then(response => response.json())
+	.then((response) => {
+
+		sorengTemp.innerHTML = response.temp
+		sorengMax_temp.innerHTML = response.max_temp
+		sorengMin_temp.innerHTML = response.min_temp
+		sorengWind_speed.innerHTML = response.wind_speed
+		sorengHumidity.innerHTML = response.humidity
+
+
+		   //    console.log(response)
+	})
+.catch(err => console.error(err));
+
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Pelling', op)
+	.then(response => response.json())
+	.then((response) => {
+
+		pellingTemp.innerHTML = response.temp
+		pellingMax_temp.innerHTML = response.max_temp
+		pellingMin_temp.innerHTML = response.min_temp
+		pellingWind_speed.innerHTML = response.wind_speed
+		pellingHumidity.innerHTML = response.humidity
+
+
+		   //    console.log(response)
+	})
+.catch(err => console.error(err));
+
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Tadong', op)
+	.then(response => response.json())
+	.then((response) => {
+
+		yanyangTemp.innerHTML = response.temp
+		yanyangMax_temp.innerHTML = response.max_temp
+		yanyangMin_temp.innerHTML = response.min_temp
+		yanyangWind_speed.innerHTML = response.wind_speed
+		yanyangHumidity.innerHTML = response.humidity
+
+
+		   //    console.log(response)
+	})
+.catch(err => console.error(err));
+
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Melli', op)
+	.then(response => response.json())
+	.then((response) => {
+
+		melliTemp.innerHTML = response.temp
+		melliMax_temp.innerHTML = response.max_temp
+		melliMin_temp.innerHTML = response.min_temp
+		melliWind_speed.innerHTML = response.wind_speed
+		melliHumidity.innerHTML = response.humidity
+
+
+		   //    console.log(response)
+	})
+.catch(err => console.error(err));
